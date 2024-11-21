0e342e10282a13139ddcd5ae8ae0aac167b47c2d
jofisjohn <jofis007@gmail.com>
2024-11-21 07:08:15 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,6 @@
 public class HelloWorld {
   public static void main(String[] args) {
     System.out.println("Hello world!! Main");
+    System.out.println("Hello world!! Main 21 11 2024");
   }
 }
